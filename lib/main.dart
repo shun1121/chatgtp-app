9e9b9eb1e35d3bea47d427636704a2bbc6f82617
SFukunishi <fukunishi.s@tsunagu-grp.jp>
2024-06-26 22:55:28 +0900

    verify certificate

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mixi_training/chat_screen.dart';
 import 'package:mixi_training/first_page.dart';
-
+import 'dart:io';
 import 'my_home_page.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
 
 class MyApp extends StatelessWidget {
